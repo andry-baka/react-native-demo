9cd59f4ab502374c69e2ec9ebca8ae71dcd26de0
Walter Ngo <codesigngo@gmail.com>
2017-10-07 14:20:55 +0700

    feat: Landing page, Flight detail

--- app/screens/FlightInfo.js
+++ app/screens/FlightInfo.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import {
-  View, Text, Button
+  View, Text, Button, TouchableOpacity, Image, Dimensions
 } from 'react-native';
 import Routes from './../config/Routes';
+import Images from './../assets/Images';
+const { width, height } = Dimensions.get('window');
 
 export default class Profile extends Component {
   constructor(props) {
@@ -15,22 +17,232 @@ export default class Profile extends Component {
         style={{
           flex: 1,
           alignItems: 'center',
-          justifyContent: 'center'
+          backgroundColor: '#8e816c'
         }}
       >
-        <Text>FlightInfo</Text>
-        <Button
-          title={'Go to Profile'}
-          onPress={() => {
-            this.props.navigation.goBack();
+        <View
+          style={{
+            flexDirection: 'row',
+            marginTop: 40,
+            justifyContent: 'space-between',
+            width: width - 50,
           }}
-        />
-        <Button
-          title={'Go to Flight Booking'}
-          onPress={() => {
-            this.props.navigation.navigate(Routes.FlightBooking);
+        >
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.goBack();
+            }}
+          >
+            <Image
+              source={Images.back}
+              style={{
+                width: 20,
+                height: 20
+              }}
+            />
+          </TouchableOpacity>
+          <Text
+            style={{
+              color: '#f3ebdf',
+              fontSize: 20,
+              fontWeight: '700'
+            }}
+          >
+            Flight Details
+          </Text>
+          <View
+            style={{
+              width: 20,
+              height: 20
+            }}
+          />
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 24
           }}
-        />
+        >
+          <View
+            style={{
+              alignItems: 'center'
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 30,
+                color: '#fff',
+                fontWeight: '700'
+              }}
+            >
+              CGK
+            </Text>
+            <Text
+              style={{
+                fontSize: 14,
+                color: '#fff',
+                fontWeight: '600'
+              }}
+            >
+              Jakarta
+            </Text>
+          </View>
+          <Image
+            source={Images._smallPlane_}
+            style={{
+              width: 113,
+              height: 24
+            }}
+          />
+          <View
+            style={{
+              alignItems: 'center'
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 30,
+                color: '#fff',
+                fontWeight: '700'
+              }}
+            >
+              SIN
+            </Text>
+            <Text
+              style={{
+                fontSize: 14,
+                color: '#fff',
+                fontWeight: '600'
+              }}
+            >
+              Singapore
+            </Text>
+          </View>
+        </View>
+
+        <View
+          style={{
+            marginTop: 20,
+            width: width - 60,
+            height: 460,
+            borderRadius: 16,
+            backgroundColor: '#fff',
+            shadowColor: '#000', shadowOffset: {width: 0, height: 1}, shadowOpacity: 0.4, shadowRadius:1.5,
+          }}
+        >
+          <View
+            style={{
+              borderTopRightRadius: 16,
+              borderTopLeftRadius: 16,
+              backgroundColor: '#000810',
+              height: 80,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              padding: 12,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center'
+              }}
+            >
+              <Image
+                source={Images.planeUp}
+                style={{
+                  width: 21,
+                  height: 15,
+                  marginHorizontal: 16
+                }}
+              />
+              <View>
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontSize: 18,
+                    fontWeight: '600'
+                  }}
+                >
+                  21:45
+                </Text>
+                <Text
+                  style={{
+                    color: '#cccccc',
+                    fontWeight: '500'
+                  }}
+                >
+                  8 Oct
+                </Text>
+              </View>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center'
+              }}
+            >
+              <View
+                style={{
+                  alignItems: 'flex-end'
+                }}
+              >
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontSize: 18,
+                    fontWeight: '600'
+                  }}
+                >
+                  00:15
+                </Text>
+                <Text
+                  style={{
+                    color: '#cccccc',
+                    fontWeight: '500'
+                  }}
+                >
+                  9 Oct
+                </Text>
+              </View>
+              <Image
+                source={Images.planeDown}
+                style={{
+                  width: 21,
+                  height: 15,
+                  marginHorizontal: 16
+                }}
+              />
+            </View>
+          </View>
+          <Image
+            source={Images.ticketBG}
+            style={{
+              width: width - 60,
+              height: 115,
+              paddingHorizontal: 30,
+              paddingVertical: 15
+            }}
+          >
+            <Image
+              source={Images.salogo}
+              style={{
+              }}
+            />
+            <View>
+              <Text>
+                We are seeing you still not do {'\n'}
+                check-in right now?
+              </Text>
+                <TouchableOpacity>
+                </TouchableOpacity>
+            </View>
+          </Image>
+        </View>
+
       </View>
     );
   }
--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import {
-  View, Text, Button
+  View, Text, Button, Image, StatusBar, Dimensions,
+  TouchableOpacity
 } from 'react-native';
 import Routes from './../config/Routes';
 import PushNotification from 'react-native-push-notification';
+import Images from './../assets/Images';
+const { width, height } = Dimensions.get('window');
 
 export default class Profile extends Component {
   constructor(props) {
@@ -23,22 +26,278 @@ export default class Profile extends Component {
         style={{
           flex: 1,
           alignItems: 'center',
-          justifyContent: 'center'
+          backgroundColor: '#8e816c'
         }}
       >
-        <Text>Profile</Text>
-        <Button
-          title={'Go to Reward'}
-          onPress={() => {
-            this.props.navigation.navigate(Routes.Reward);
+        <StatusBar
+          barStyle="light-content"
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            marginTop: 40,
+            justifyContent: 'space-between',
+            width: width - 50
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.navigate(Routes.Reward)
+            }}
+          >
+            <Image
+              source={Images.reward}
+              style={{
+                width: 42,
+                height: 42
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.navigate(Routes.Notifications)
+            }}
+          >
+            <Image
+              source={Images.bell}
+              style={{
+                width: 42,
+                height: 42
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+        <Image
+          source={Images.logo}
+          style={{
+            marginTop: 20,
+            width: 622 * 0.46875,
+            height: 561 * 0.46875,
+            resizeMode: 'contain'
           }}
         />
-        <Button
-          title={'Go to Flight Info'}
+        <View>
+          <Text
+            style={{
+              color: '#fff'
+            }}
+          >
+            Welcome,
+          </Text>
+          <Text
+            style={{
+              color: '#fff',
+              fontSize: 30,
+              fontWeight: '700'
+            }}
+          >
+            Andry Baka
+          </Text>
+        </View>
+        <View
+          style={{
+            marginTop: 10,
+            backgroundColor: '#000',
+            width,
+            height: 60,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              textAlign: 'center',
+              backgroundColor: 'transparent'
+            }}
+          >
+            Don’t forget you will have flight{'\n'}
+            tomorrow evening
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          onPress={() => {
+          }}
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginTop: 16,
+            borderRadius: 8,
+            backgroundColor: '#6d6353',
+            paddingHorizontal: 34,
+            paddingVertical: 12
+          }}
+        >
+          <Image source={Images.add} style={{width: 25, height: 25}} />
+          <Text style={{ marginLeft: 8, color: '#fff', fontWeight: '600' }}>Add New Flight</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            marginTop: 26,
+            width: 350,
+            height: 60,
+            backgroundColor: '#fff',
+            borderRadius: 12,
+            shadowColor: '#000', shadowOffset: {width: 0, height: 1}, shadowOpacity: 0.4, shadowRadius:1.5,
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'row'
+          }}
           onPress={() => {
             this.props.navigation.navigate(Routes.FlightInfo);
           }}
-        />
+        >
+          <Image
+            source={Images.salogo}
+            style={{
+              width: 55 * 0.46875,
+              height: 73 * 0.46875,
+            }}
+          />
+          <View
+            style={{
+              marginLeft: 15
+            }}
+          >
+            <Text
+              style={{
+                color: '#606060',
+                fontSize: 13,
+                fontWeight: '500'
+              }}
+            >
+              Business Class GA-01
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center'
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontWeight: '600'
+                }}
+              >
+                CGK
+              </Text>
+              <Image
+                source={Images.smallPlane}
+                style={{
+                  width: 14,
+                  height: 14
+                }}
+              />
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontWeight: '600'
+                }}
+              >
+                SIN
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center'
+              }}
+            >
+              <Image
+                source={Images.checked}
+                style={{
+                  width: 12,
+                  height: 12
+                }}
+              />
+              <Text
+                style={{
+                  color: '#606060',
+                  fontSize: 13,
+                  fontWeight: '500',
+                  marginLeft: 4,
+                }}
+              >
+                Check-in
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              width: 1,
+              height: 46,
+              backgroundColor: '#e7e7e7',
+              marginHorizontal: 10
+            }}
+          />
+          <View
+            style={{
+              alignItems: 'center'
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 13,
+                color: '#616161'
+              }}
+            >
+              Seat
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: '600'
+              }}
+            >
+              4A
+            </Text>
+            <Text
+              style={{
+                color: '#80ae54',
+                fontSize: 10
+              }}
+            >
+              Gate 5
+            </Text>
+          </View>
+          <View
+            style={{
+              width: 1,
+              height: 46,
+              backgroundColor: '#e7e7e7',
+              marginHorizontal: 10
+            }}
+          />
+          <View>
+            <Text
+              style={{
+                fontSize: 13,
+                color: '#616161'
+              }}
+            >
+              8 Oct
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: '600'
+              }}
+            >
+              21:30
+            </Text>
+            <Text
+              style={{
+                color: '#80ae54',
+                fontSize: 10
+              }}
+            >
+              On Schedule
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
