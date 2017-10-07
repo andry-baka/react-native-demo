94e4159567805dfb713fad87808e4384c527d762
Son Nguyen <nvs2394@gmail.com>
2017-10-07 14:55:19 +0700

    Add HistoryListItem

---
+++ app/components/HistoryPointList.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import {
+  View, Text, ListView, Image, StyleSheet
+} from 'react-native';
+import Routes from './../config/Routes';
+
+
+const HistoryItem = (props) => {
+  const icon  = props.item.status === 'done' ? require(`../assets/icons/coin-icon.png`) : require(`../assets/icons/spend-icon.png`)
+  return (
+    <View style={{flexDirection: 'row', borderBottomColor: '#878787', borderBottomWidth: 1, padding: 20, justifyContent: 'space-between'}}>
+      <Image 
+        source={icon}
+        style= {{
+          width: 61 * 0.6,
+          height: 61 * 0.6,
+          resizeMode: 'contain'
+        }}
+      />
+      <View style={{paddingLeft: 10}}>
+        <Text style={{fontWeight: 'bold', color: '#231f20', fontStyle: 'italic'}}>Spending on Sakuara Restaurent</Text>
+        <Text style={{fontSize: 12}}>07 Oct . Invoice number 09283</Text>
+      </View>
+      <Text style={ props.item.status === 'done' ? styles.pointStatusDone : styles.pointStatus}>
+        {props.item.status !== 'done' ? props.item.point : `+ ${props.item.point} `}
+      </Text>
+    </View>
+  )
+}
+
+class HistoryList extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        {
+          status: 'done',
+          point: 10
+        },
+        {
+          status: 'pending',
+          point: 30
+        },
+        {
+          status: 'pending',
+          point: 10
+        },
+        {
+          status: 'done',
+          point: 50
+        }
+      ]),
+    };
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <HistoryItem item={rowData}/>}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  pointStatusDone: {
+    paddingLeft: 10,
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#fea000',
+    minWidth: 60,
+    textAlign: 'right'
+  },
+  pointStatus: {
+    paddingLeft: 10,
+    fontSize: 17,
+    fontWeight: 'bold',
+    minWidth: 60,
+    textAlign: 'right'
+  },
+});
+
+
+export default HistoryList
+
--- app/screens/HistoryPoint.js
+++ app/screens/HistoryPoint.js
@@ -1,34 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
 import {
-  View, Text, Button
+  View, Text, Button, StyleSheet
 } from 'react-native';
+
 import Routes from './../config/Routes';
+import HistoryPointListComponent from '../components/HistoryPointList'
+
+
+const FirstRoute = () => (
+  <View style={[ styles.container, { backgroundColor: '#fff' } ]}>
+    <HistoryPointListComponent/>
+  </View>
+)
+const SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
 
 class HistoryPoint extends Component {
   constructor(props) {
     super(props);
   }
 
+  state = {
+    index: 0,
+    routes: [
+      { key: '1', title: 'Point History' },
+      { key: '2', title: 'Partnerhip' },
+    ],
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderHeader = props => <TabBar {...props} />;
+
+  _renderScene = SceneMap({
+    '1': FirstRoute,
+    '2': SecondRoute,
+  });
+
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}
-      >
-        <Text>HistoryPoint</Text>
-        <Button
-          title={'Go to Profile'}
-          onPress={() => {
-            this.props.navigation.goBack();
-          }}
-        />
-      </View>
+      <TabViewAnimated
+        style={styles.container}
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderHeader={this._renderHeader}
+        onIndexChange={this._handleIndexChange}
+      />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tabBarStyle: {
+    backgroundColor: '#000'
+  }
+});
+
 
 export default HistoryPoint
 
